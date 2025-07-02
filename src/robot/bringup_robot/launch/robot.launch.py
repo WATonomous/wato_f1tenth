599b21c33e32c7ea4fef0db60a404b37c4495123
Rodney <rodneydong156@gmail.com>
2025-07-01 22:44:10 -0400

    test ubuntu

--- src/robot/bringup_robot/launch/robot.launch.py
+++ src/robot/bringup_robot/launch/robot.launch.py
@@ -3,6 +3,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+#Test
 
 import os
 
