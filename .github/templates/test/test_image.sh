8af5ecc950581219d99400c09fb1ec2b701a8ed3
Derek Lin <derekhongdalin@gmail.com>
2025-07-09 03:13:48 -0400

    do what warning said

--- .github/templates/test/test_image.sh
+++ .github/templates/test/test_image.sh
@@ -2,5 +2,5 @@
 set -e
 
 docker pull -q $IMAGE:$TAG
-docker run $IMAGE:$TAG /bin/bash -c "source /home/bolty/ament_ws/install/setup.bash; colcon test; colcon test-result --verbose"
-# docker run $IMAGE:$TAG /bin/bash -c "source /opt/ros/\$ROS_DISTRO/setup.bash; source \$WATONOMOUS_INSTALL/setup.bash; colcon test; colcon test-result --verbose"
+# docker run $IMAGE:$TAG /bin/bash -c "source /home/bolty/ament_ws/install/setup.bash; colcon test; colcon test-result --verbose"
+docker run $IMAGE:$TAG /bin/bash -c "source /opt/ros/\$ROS_DISTRO/setup.bash; source \$WATONOMOUS_INSTALL/setup.bash; colcon test; colcon test-result --verbose"
